478175b194224661bfcc5e8f568915a86ab95797
aaronbyte <63644427+aaronbyte@users.noreply.github.com>
2023-02-06 22:05:12 +0800

    Update test.py
    Added shortened columns and other changes to work with new column names

--- test.py
+++ test.py
@@ -7,11 +7,9 @@ Created on Tue Jan 31 10:48:44 2023
 
 import numpy as np
 import pandas as pd
-# =============================================================================
-# import seaborn as sb
-# import matplotlib.pyplot as plt
-# sb.set()
-# =============================================================================
+import seaborn as sb
+import matplotlib.pyplot as plt
+sb.set()
 
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
@@ -44,16 +42,22 @@ print(productDF.head())
 #assign range for customisation likelihood
 #assign range for car customisation fee
 
+#sb.heatmap(productDF.isnull(),yticklabels=False,cbar=False,cmap='Blues')
+
+# Renaming dataframe columns
+productDF.columns = ["No.", "Age", "Gender", "Category", "MarriageStatus", "FactorsPurchase", "FreeCustomization", "ExteriorComponents", "InteriorComponents", "WTSCustomization", "WantOwnPersonalization", "WTSPersonalization", "PersonalizationJob"]
+
 #Creating dataframes for each header
 productDF=productDF.drop(productDF.columns[0], axis=1)
-productDF['factors'] = productDF['Which of these factors are important to you when deciding which car to purchase?'].str.split(";", n = -1, expand = False)
-productDF['external'] = productDF['Which of the following exterior components would you choose to customise (texture, layout, size, etc)? '].str.split(";", n = -1, expand = False)
-productDF['internal'] = productDF['Which of the following interior components would you choose to customise (texture, layout, size, etc)? '].str.split(";", n = -1, expand = False)
+productDF['factors'] = productDF['FactorsPurchase'].str.split(";", n = -1, expand = False)
+productDF['external'] = productDF['ExteriorComponents'].str.split(";", n = -1, expand = False)
+productDF['internal'] = productDF['InteriorComponents'].str.split(";", n = -1, expand = False)
 
 #length of how many customisations of car
 productDF['fact_length'] = productDF['factors'].str.len()
 productDF['ext_length'] = productDF['external'].str.len()
 productDF['int_length'] = productDF['internal'].str.len()
 
-#Saving dataframe into excel file to analyse on R
+
+
 productDF.to_excel('stringed_survey.xlsx', index=False)
